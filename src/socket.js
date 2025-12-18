9391832d6629722c1e39667994a7b3b3b074b07d
fix: validate product amount <>
2025-12-18 16:53:52 +0700

    fix server

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,10 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:4000', {
-  transports: ['websocket'],
-});
+const socket = io(
+  'https://private-chat-server-1.onrender.com',
+  {
+    transports: ['websocket'],
+  }
+);
 
 export default socket;
